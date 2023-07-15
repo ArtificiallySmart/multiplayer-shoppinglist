c6532e29aceec3bbbea5648e03d1ecddecbd1b8f
Bryan <bryanparcival@gmail.com>
2023-07-15 16:03:45 +0200

    feat: added nav

---
+++ src/screens/home.tsx
@@ -0,0 +1,26 @@
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { View, Text, Button } from "react-native";
+import { RootStackParamList } from "../../App";
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+type Props = {
+  navigation: HomeScreenNavigationProp;
+};
+
+function HomeScreen({ navigation }: Props) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+export default HomeScreen;
